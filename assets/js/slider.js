e560657da9a6d4d69bc36da4b080c14c98360ad6
darocode <derodriguez074@misena.edu.co>
2022-07-24 20:02:54 -0500

    Taller0

---
+++ assets/js/slider.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider_section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+//slider.insertAdjacentElement('afterbegin', sliderSectionLast);
